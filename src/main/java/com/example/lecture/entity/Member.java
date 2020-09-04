06d876ad1dc449d65c7a3fb6a4be91011dc5a83a
zeeeeeno <15481079lee@gmail.com>
2020-09-04 18:23:41 +0900

    2020-09-04 mini project

--- src/main/java/com/example/lecture/entity/Member.java
+++ src/main/java/com/example/lecture/entity/Member.java
@@ -10,7 +10,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
